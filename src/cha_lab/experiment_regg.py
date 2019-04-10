6bc1d58b803528f4c1cc009ae0f045dd4cad9658
tea7010 <tea7010@users.noreply.github.com>
2019-04-10 19:10:47 +0900

    ちょっと構造変更

---
+++ src/cha_lab/experiment_regg.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+sys.path.append('D://isid_phm/src/')
+
+if __name__ == "__main__":
+    from run_reggression_model import run_reggression_model
+
+    NUM = 20
+    experiment_df = pd.DataFrame(index=range(NUM))
+    for i in range(NUM):
+        params = {
+            'regenarate': True,
+            'train_cutoff': True,
+            'num_resample_train': i+1,
+            'scaling': True,
+            'use_model': 'SVR',
+            'model_params': {'gamma': 'auto'},
+            'submit': False
+        }
+
+        valid_score = run_reggression_model(params)
+
+        experiment_df.loc[i, 'Experiment_No'] = i
+        experiment_df.loc[i, 'score'] = valid_score
+
+    experiment_df.to_csv('result.csv')
