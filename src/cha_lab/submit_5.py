35862546098773eef8f80ab12473adbdb2ce98d7
tea7010 <tea7010@users.noreply.github.com>
2019-04-16 19:38:17 +0900

    VM使ってみた

---
+++ src/cha_lab/submit_5.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sys
+sys.path.append('D://isid_phm/src/')
+
+if __name__ == "__main__":
+    from run_reggression_model import run_reggression_model
+
+    params = {
+        'regenarate': True,
+        'train_cutoff': True,
+        'num_resample_train': 5,
+        'scaling': True,
+        'use_model': 'SVR',
+        'model_params': {'gamma': 'auto'},
+        'submit': True
+    }
+
+    valid_score = run_reggression_model(params)
